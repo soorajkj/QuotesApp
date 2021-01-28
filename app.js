89b2907034fd79e1c13da46b5ea4c58ffcc87313
soorajkj <soorajkj46@gmail.com>
2021-01-28 20:37:12 +0530

    added local storage feature

--- app.js
+++ app.js
@@ -2,19 +2,46 @@
 const container = document.querySelector('.container');
 const btn = document.querySelector('.btn-generate');
 
-
-btn.addEventListener('click', () =>{
-    getQuotes().then(data =>{
-        if(data.author){
+if(localStorage.getItem('q')){
+    const lq1 = localStorage.getItem('q');
+    const lq2 = JSON.parse(lq1);
+    
+    if(lq2.author){
         container.innerHTML = 
         `
         <div class="q-wrapper">
         <div class="q-head">
-        <blockquote class="quote"><span>"</span>${data.text}</blockquote>
+        <blockquote class="quote"><span>"</span>${lq2.text}</blockquote>
         </div>
-        <p class="author">${data.author}</p>
+        <p class="author">${lq2.author}</p>
         </div>
         `; 
+        
+    }
+    else{
+        container.innerHTML =
+        `<div class="q-wrapper">
+        <div class="q-head">
+        <blockquote class="quote"><span>"</span>${lq2.text}</blockquote>
+        </div> ` 
+    }
+}
+
+
+
+btn.addEventListener('click', () =>{
+    getQuotes().then(data =>{
+        if(data.author){
+            container.innerHTML = 
+            `
+            <div class="q-wrapper">
+            <div class="q-head">
+            <blockquote class="quote"><span>"</span>${data.text}</blockquote>
+            </div>
+            <p class="author">${data.author}</p>
+            </div>
+            `; 
+            
         }
         else{
             container.innerHTML =
@@ -23,12 +50,15 @@ btn.addEventListener('click', () =>{
             <blockquote class="quote"><span>"</span>${data.text}</blockquote>
             </div> ` 
         }
+
+        localStorage.setItem('q', JSON.stringify(data));
     })
     .catch(err =>{
         console.log(err);
     });
 
 });
+
 const getQuotes = async () => {
     const response = await fetch("https://type.fit/api/quotes");
     const data = await response.json();
